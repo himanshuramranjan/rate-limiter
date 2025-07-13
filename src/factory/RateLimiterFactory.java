53a549bdde03091c7c9ffc5997d2906461af4197
Himanshuram Ranjan <himanshuramranjan@Himanshurams-MacBook-Air.local>
2025-07-13 22:00:27 +0530

    code refactoring

--- src/factory/RateLimiterFactory.java
+++ src/factory/RateLimiterFactory.java
@@ -12,23 +12,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RateLimiterFactory {
+
+    private RateLimiterFactory() {}
+
     public static RateLimiter createRateLimiter(Map<String, RateLimiterConfig> apiConfigs, String strategyType) {
         Map<String, RateLimiterStrategy> strategyMap = new HashMap<>();
 
-        RateLimiterStrategy strategy;
-        switch (strategyType.toLowerCase()) {
-            case "fixed":
-                strategy = new FixedWindowStrategy(apiConfigs);
-                break;
-            case "sliding":
-                strategy = new SlidingWindowStrategy(apiConfigs);
-                break;
-            case "token":
-                strategy = new TokenBucketStrategy(apiConfigs);
-                break;
-            default:
-                throw new IllegalArgumentException("Unsupported strategy: " + strategyType);
-        }
+        RateLimiterStrategy strategy = switch (strategyType.toLowerCase()) {
+            case "fixed" -> new FixedWindowStrategy(apiConfigs);
+            case "sliding" -> new SlidingWindowStrategy(apiConfigs);
+            case "token" -> new TokenBucketStrategy(apiConfigs);
+            default -> throw new IllegalArgumentException("Unsupported strategy: " + strategyType);
+        };
 
         for (String api : apiConfigs.keySet()) {
             strategyMap.put(api, strategy); // You can also assign different strategies per API if needed
