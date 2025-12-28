248e05e97f40006b796b2cce8a01a6bf40c1e15f
Himanshuram Ranjan <himanshuramr@gmail.com>
2025-12-28 19:59:48 +0530

    updated the demo example

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import config.RateLimiterConfig;
 import factory.RateLimiterStrategyFactory;
-import model.ApiRateLimiter;
+import service.ApiRateLimiter;
 import model.RateLimitResponse;
-import model.RateLimiter;
 import strategy.RateLimiterStrategy;
 
 import java.util.HashMap;
@@ -14,25 +13,10 @@ import static enums.StrategyType.SLIDING;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        Map<String, RateLimiterConfig> userApiConfigs = Map.of(
-                "/api/user/login", new RateLimiterConfig(5, 10, null),
-                "/api/user/logout", new RateLimiterConfig(50, 10, null)
-        );
-
-        Map<String, RateLimiterConfig> orderApiConfigs = Map.of(
-                "/api/order", new RateLimiterConfig(3, 5, null)
-        );
-
-        Map<String, RateLimiterStrategy> strategyMap = new HashMap<>();
-
-        RateLimiterStrategy fixedWindowStrategy = RateLimiterStrategyFactory.createRateLimiter(userApiConfigs, FIXED);
-        RateLimiterStrategy slidingWindowStrategy = RateLimiterStrategyFactory.createRateLimiter(orderApiConfigs, SLIDING);
-
-        strategyMap.put("/api/user/login", fixedWindowStrategy);
-        strategyMap.put("/api/user/logout", fixedWindowStrategy);
-        strategyMap.put("/api/order", slidingWindowStrategy);
-
-        RateLimiter rateLimiter = ApiRateLimiter.getInstance(strategyMap);
+        ApiRateLimiter rateLimiter = ApiRateLimiter.getInstance();
+        rateLimiter.registerApi("/api/user/login", new RateLimiterConfig(5, 10, null), FIXED);
+        rateLimiter.registerApi("/api/user/logout", new RateLimiterConfig(50, 10, null), FIXED);
+        rateLimiter.registerApi("/api/order", new RateLimiterConfig(3, 5, null), SLIDING);
 
         for (int i = 0; i < 15; i++) {
             RateLimitResponse res = rateLimiter.allowRequest("/api/user/login");
