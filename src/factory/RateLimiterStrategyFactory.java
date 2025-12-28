2e10e912944535f8fe880322085d071be19f56d6
Himanshuram Ranjan <himanshuramr@gmail.com>
2025-12-28 19:58:07 +0530

    updated factory to retrun the instances of strategies

--- src/factory/RateLimiterStrategyFactory.java
+++ src/factory/RateLimiterStrategyFactory.java
@@ -7,18 +7,16 @@ import strategy.RateLimiterStrategy;
 import strategy.SlidingWindowStrategy;
 import strategy.TokenBucketStrategy;
 
-import java.util.Map;
-
 public class RateLimiterStrategyFactory {
 
     private RateLimiterStrategyFactory() {}
 
-    public static RateLimiterStrategy createRateLimiter(Map<String, RateLimiterConfig> apiConfigs, StrategyType strategyType) {
+    public static RateLimiterStrategy getStrategy(StrategyType strategyType) {
 
         return switch (strategyType) {
-            case FIXED -> new FixedWindowStrategy(apiConfigs);
-            case SLIDING -> new SlidingWindowStrategy(apiConfigs);
-            case TOKEN -> new TokenBucketStrategy(apiConfigs);
+            case FIXED -> FixedWindowStrategy.getInstance();
+            case SLIDING -> SlidingWindowStrategy.getInstance();
+            case TOKEN -> TokenBucketStrategy.getInstance();
         };
     }
 }
